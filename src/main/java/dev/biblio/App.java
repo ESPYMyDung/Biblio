9db215faf49d206f899d94daf2d7bc8aa15fad26
ESPY My Dung <ngocdiepespy@sfr.fr>
2019-04-04 15:16:17 +0200

    tp jpa : tp1

--- src/main/java/dev/biblio/App.java
+++ src/main/java/dev/biblio/App.java
@@ -1,5 +1,16 @@
 package dev.biblio;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+
+
+//import dev.Livre;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //creer un EntityManagerFactory
+    	EntityManagerFactory factory = Persistence.createEntityManagerFactory("Biblio");
+    	
+    	//creer une unite de travail
+    	EntityManager travail = factory.createEntityManager();
+    	
+    	//Livre bouquin = travail.find(Livre.class,4);
+    	//System.out.println(bouquin.getAuteur());
+
+    	
+    	TypedQuery<Livre> requete = travail.createQuery(
+    			"select l from Livre l where TITRE=:ref", Livre.class);
+    	requete.setParameter("ref", "Germinal");
+    	
+    	List<Livre> liste = requete.getResultList();
+
+    	liste.forEach(bouquin -> {
+			System.out.println(bouquin.getId() + " - "  + bouquin.getTitre() + " - " + bouquin.getAuteur());
+		});
+    			
+    	// fin
+    	travail.close();
+    	factory.close();
+    	
     }
 }
--- src/main/java/dev/biblio/Livre.java
+++ src/main/java/dev/biblio/Livre.java
@@ -1,15 +1,54 @@
 package dev.biblio;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Table(name="livre")
 public class Livre
 {
 	//attribut
 	@Id
-	@Table
+	@Column(name="ID")
 	private int id;
+	
+	@Column(name="TITRE")
+	private String titre;
+	
+	@Column(name="AUTEUR")
+	private String auteur;
+	
+	//getter
+	public String getTitre()
+	{
+		return titre;
+	}
+	
+	public String getAuteur()
+	{
+		return auteur;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+	
+	/*
+	//setter
+	public void setTitre(String titre)
+	{
+		this.titre = titre;
+	}
+	
+	public void setAuteur(String auteur)
+	{
+		this.auteur = auteur;
+	}
+	*/
+	
+	
 
 }
